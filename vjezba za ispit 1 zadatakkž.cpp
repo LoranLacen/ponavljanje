8eba100e28a5e73d2a0e059e175257bb26b59473
LoranLacen <58933094+LoranLacen@users.noreply.github.com>
2019-12-16 09:12:53 +0100

    Add files via upload

---
+++ vjezba za ispit 1 zadatakkž.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    for (int i=1;i<=20;i++){
+        if (i==20){
+           cout<<i<<".";
+           }else{
+           cout<<i<<",";
+        }
+}
+    
+    
+system ("pause");
+return 0;
+}
---
+++ vjezba za ispit 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    for (int i=0;i<=4;i++){
+    for (int n=0;n<=9;n++){
+cout<<i<<"-"<<n<<endl;
+}
+}
+cout<<"5-0";
+
+system ("pause");
+return 0;
+}
